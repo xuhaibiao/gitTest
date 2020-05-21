43fbbcc0ac2b621a54e3676ea509c89baf36b51b
xuhaibiao <854186712@qq.com>
2020-05-21 14:27:26 +0800

    master new code1

--- src/code1.java
+++
@@ -1,9 +0,0 @@
-
-/**
- * @author HaibiaoXu
- * @date Create in 13:55 2020/5/21
- * 
- */
-public class code1 {
-    private int first;
-}
---
+++ src/master_newCode1.java
@@ -0,0 +1,9 @@
+/**
+ * @author HaibiaoXu
+ * @date Create in 14:17 2020/5/21
+ * @modified By
+ */
+
+public class master_newCode1 {
+
+}
